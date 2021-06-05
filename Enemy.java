185d99aab46d4d695e467546c85add19fd1eba3b
ObnoxiousOliver <maxhd2005@gmail.com>
2021-06-05 21:42:46 +0200

    new random Mode Added

--- Enemy.java
+++ Enemy.java
@@ -6,11 +6,15 @@ public class Enemy extends Actor
     int health;
     int speed;
     int timer = 0;
-    
-    public Enemy(int health, int speed)
+    boolean randomMotion;
+    int direction;
+
+    public Enemy(int health, int speed, boolean randomMotion)
     {
         this.health = health;
         this.speed = speed;
+        this.randomMotion = randomMotion;
+        this.direction = getRandomNum(0, 360);
     }
     
     public void act() 
@@ -30,19 +34,67 @@ public class Enemy extends Actor
     
     private void fly()
     {
-        setLocation(getX() + this.speed, getY());
-        
-        if (getX() > 768 || getX() < 32)
+        if (randomMotion)
         {
-            this.speed *= -1;
-            setLocation(getX(), getY() + 32);
+            // get x y based of an angle and a distance (speed)
+            double x = getX() + this.speed * Math.cos(direction);
+            double y = getY() + this.speed * Math.sin(direction);
+
+            setLocation((int)x, (int)y);
+
+            // set random directon when enemy hits border
+            if (getX() > 768)
+            {
+                this.direction = getRandomNum(90, 270);
+
+                // add padding left
+                setLocation(getX() - 16, getY());
+            }
+            if (getX() < 32)
+            {
+                this.direction = getRandomNum(270, 450);
+
+                // add padding right
+                setLocation(getX() + 16, getY());
+            }
+            
+            if (getY() > 568)
+            {
+                this.direction = getRandomNum(180, 360);
+
+                // add padding bottom
+                setLocation(getX(), getY() - 16);
+            }
+            if (getY() < 32)
+            {
+                this.direction = getRandomNum(180, 360);
+
+                // add padding top
+                setLocation(getX(), getY() + 16);
+            }
         }
-        
-        if (getY() > 568)
+        else
         {
-            setLocation(32, 32);
+            // Soft borders
+            setLocation(getX() + this.speed, getY());
+
+            if (getX() > 768 || getX() < 32)
+            {
+                this.speed *= -1;
+                setLocation(getX(), getY() + 32);
+            }
+            
+            if (getY() > 568)
+            {
+                setLocation(32, 32);
+            }
         }
-        
-        // Soft borders in if statements above
+
+    }
+
+    public int getRandomNum(int start, int end)
+    {
+        int normal = Greenfoot.getRandomNumber(end - start + 1);
+        return normal + start;
     }
 }
--- Space.java
+++ Space.java
@@ -8,17 +8,19 @@ public class Space extends World
         // Create a new world with 800x600 cells with a cell size of 1x1 pixels
         super(800, 600, 1);
         spawnPlayer(400, 534);
+        prepare();
     }
-        
+
     public void act()
     {
         if (getObjects(Enemy.class).isEmpty())
         {   
             getObjects(Player.class).get(0).setLocation(400, 534);
             spawnEnemies(getRandomNum(1, 8),
-                         getRandomNum(1, 6),
-                         getRandomNum(25, 75),
-                         getRandomNum(1, 4));            
+                getRandomNum(1, 6),
+                getRandomNum(25, 75),
+                getRandomNum(2, 4),
+                getRandomNum(0, 1) == 1);
         }
     }   
 
@@ -30,7 +32,7 @@ public class Space extends World
     /*
      * Spawn a given matrix of enemies and give them an amount of HP and speed.
      */
-    private void spawnEnemies(int numToSpawn, int numRows, int healthPoints, int speed)
+    private void spawnEnemies(int numToSpawn, int numRows, int healthPoints, int speed, boolean randomMotion)
     {
         int posY = 32;
         for (int row = 0; row < numRows; row++)
@@ -39,9 +41,9 @@ public class Space extends World
             List<Enemy> enemies = new ArrayList<Enemy>();
             for (int enemyCount = 0; enemyCount < numToSpawn; enemyCount++)
             {
-                enemies.add(new Enemy(healthPoints, speed));
+                enemies.add(new Enemy(healthPoints, speed, randomMotion));
             }
-        
+
             // divide the screen in segments of the count of enemies
             int intervalX = 800 / enemies.size();
             int step = intervalX / 2;
@@ -50,15 +52,23 @@ public class Space extends World
                 addObject(enemy, step, posY);
                 step += intervalX;
             }
-            
+
             posY += 64;
         }
-        
+
     }
-    
+
     public int getRandomNum(int start, int end)
     {
         int normal = Greenfoot.getRandomNumber(end - start + 1);
         return normal + start;
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }
